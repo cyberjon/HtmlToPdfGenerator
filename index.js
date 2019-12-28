875194c130d396b1b034912965f4534b5c96f5ad
cyberjon <cyberjon09@gmail.com>
2019-12-28 11:03:20 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('./src/server');
 const http = require('http');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 http.createServer(app).listen(port);
 console.log(`Server listening on port ${port}`);
